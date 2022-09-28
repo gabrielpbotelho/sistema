5ed3483ddd4062f234342cc1a2d009b7dcc39b31
gabrielpbotelho <gabrielp.botelho@gmail.com>
2022-09-27 22:49:54 -0300

    Initial commit

---
+++ sistema/src/main/java/br/com/biel/sistema/SistemaApplication.java
@@ -0,0 +1,14 @@
+package br.com.biel.sistema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaApplication.class, args);
+		System.out.println("iifgghhghgh");
+	}
+
+}
---
+++ sistema/src/test/java/br/com/biel/sistema/SistemaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.biel.sistema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
