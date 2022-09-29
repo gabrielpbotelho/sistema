e9147ad76c2ca537d40d2287bf9a08461f0ae547
gabrielpbotelho <gabrielp.botelho@gmail.com>
2022-09-28 21:14:30 -0300

    adicionando o hello.html e o HelloController.java

---
+++ sistema/src/main/java/br/com/biel/sistema/controllers/HelloController.java
@@ -0,0 +1,14 @@
+package br.com.biel.sistema.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(){
+
+        return "hello";
+    }
+}
