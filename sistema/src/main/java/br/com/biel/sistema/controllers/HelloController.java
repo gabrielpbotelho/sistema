f52261941ba18768a6228bd0ab694c25a3229028
gabrielpbotelho <gabrielp.botelho@gmail.com>
2022-10-01 18:34:41 -0300

    primeiro metodo enviando dados pro html por parametro

--- sistema/src/main/java/br/com/biel/sistema/controllers/HelloController.java
+++ sistema/src/main/java/br/com/biel/sistema/controllers/HelloController.java
@@ -3,12 +3,14 @@ package br.com.biel.sistema.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class HelloController {
 
     @GetMapping("/hello")
-    public String hello(){
-
+    public String hello(HttpServletRequest request){
+        request.setAttribute("nome","Bolete");
         return "hello";
     }
 }
